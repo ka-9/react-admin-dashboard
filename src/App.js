acbb96793ed9dc014626500f40ea44bf9bebb7d5
Kevin Aoun <kevin.w.aoun@gmail.com>
2023-01-14 23:39:46 +0200

    Modified properties to match current theme color

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { FieldSettings, FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import { Navbar, Footer, Sidebar, Theme, Settings } from './components'
+import { Navbar, Footer, Sidebar, Theme, Settings, ThemeSettings } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor } from './pages'
 import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
-    const { activeMenu } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
   return (
     <div>
         <Router>
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
                     <TooltipComponent content='Settings' position='Top'>
-                        <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                        style={{ background: 'blue', borderRadius: '50%' }}
+                        <button 
+                        type='button' 
+                        className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                        style={{ background: currentColor, borderRadius: '50%' }}
+                        onClick={()=>{setThemeSettings(true)}}
                         >
                             <FiSettings />
                         </button>
@@ -41,6 +44,9 @@ const App = () => {
                     </div>
 
                     <div>
+
+                        {themeSettings && <ThemeSettings />}
+
                         <Routes>
                             {/* Dashboard */}
                             <Route path='/' element={<Ecommerce />} />
