821772ec8ed257945e8b7dd1465b11a82f1ef588
Kevin Aoun <kevin.w.aoun@gmail.com>
2023-01-14 23:45:39 +0200

    Fixed dark mode toggles

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
-    const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
         <Router>
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
@@ -37,7 +37,7 @@ const App = () => {
                 )}
 
                 <div className={
-                    `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                 }>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                         <Navbar />
