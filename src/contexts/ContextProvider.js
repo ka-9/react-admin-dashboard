63a367ed419afd535fc9d52e103960e88c641aa4
Kevin Aoun <kevin.w.aoun@gmail.com>
2023-01-14 23:38:53 +0200

    Added theme settings into the app context variables

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,7 +11,21 @@ const initialState = {
 export const ContextProvider =({ children })=>{
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
-    const [screenSize, setScreenSize] = useState(undefined)
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+
+    const setMode = (mode) => {
+        setCurrentMode(mode);
+        localStorage.setItem('themeMode', mode);
+    }
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+        setThemeSettings(false);
+    }
 
     const handleClick = (clicked) => {
         setIsClicked({...initialState, [clicked]:[!clicked]});
@@ -20,17 +34,17 @@ export const ContextProvider =({ children })=>{
     return (
         <StateContext.Provider
                 value={{ 
-                    activeMenu,
-                    setActiveMenu,
-                    isClicked,
-                    setIsClicked,
+                    activeMenu, setActiveMenu,
+                    isClicked, setIsClicked,
                     handleClick,
-                    screenSize,
-                    setScreenSize,
+                    screenSize, setScreenSize,
+                    currentColor, setColor,
+                    currentMode, setMode,
+                    themeSettings, setThemeSettings,
                 }}>
                 { children }
         </StateContext.Provider>
     )
 }
 
-export const useStateContext =()=> useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);
